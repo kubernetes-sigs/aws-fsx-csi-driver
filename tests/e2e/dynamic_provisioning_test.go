9c2c3d2d7eb77d79dc9850b74eb61bbd8051e0ba
yiweng <yiweng@amazon.com>
2020-09-14 15:33:26 -0400

    Add support to Lustre backup

--- tests/e2e/dynamic_provisioning_test.go
+++ tests/e2e/dynamic_provisioning_test.go
@@ -158,7 +158,7 @@ var _ = Describe("[fsx-csi-e2e] Dynamic Provisioning with s3 data repository", f
 						Parameters: map[string]string{
 							"subnetId":         subnetId,
 							"securityGroupIds": strings.Join(securityGroupIds, ","),
-							"autoImportPolicy": "NEW_CHANGED"
+							"autoImportPolicy": "NEW_CHANGED",
 							"s3ImportPath":     fmt.Sprintf("s3://%s", bucketName),
 							"s3ExportPath":     fmt.Sprintf("s3://%s/export", bucketName),
 						},
