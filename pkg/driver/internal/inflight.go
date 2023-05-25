35e5340bf183152815668e404c65b716f4fc7876
Jacob Wolf <jacwolf@amazon.com>
2023-05-25 18:35:17 +0000

    Update log functions

--- pkg/driver/internal/inflight.go
+++ pkg/driver/internal/inflight.go
@@ -70,5 +70,5 @@ func (db *InFlight) Delete(key string) {
 	defer db.mux.Unlock()
 
 	delete(db.inFlight, key)
-	klog.V(4).InfoS("Node Service: volume operation finished", "key", key)
+	klog.V(4).InfoS("Volume operation finished", "key", key)
 }
