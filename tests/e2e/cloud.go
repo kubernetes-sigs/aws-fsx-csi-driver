1ca9b7a63897015912b030329f5a454aa26264fe
Nate Pietraszak <nppietra@amazon.com>
2025-08-01 23:41:49 +0000

    Update e2e tests to use aws-sdk-go-v2

--- tests/e2e/cloud.go
+++ tests/e2e/cloud.go
@@ -15,26 +15,25 @@ limitations under the License.
 package e2e
 
 import (
+	"context"
 	"fmt"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	ec2types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	fsx "sigs.k8s.io/aws-fsx-csi-driver/pkg/cloud"
 )
 
 type cloud struct {
-	ec2client *ec2.EC2
-	s3client  *s3.S3
+	ec2client *ec2.Client
+	s3client  *s3.Client
 	fsx.Cloud
 }
 
 func NewCloud(region string) *cloud {
-	config := &aws.Config{
-		Region: aws.String(region),
-	}
-	sess := session.Must(session.NewSession(config))
+	config, _ := config.LoadDefaultConfig(context.Background(), config.WithRegion(region))
 
 	c, err := fsx.NewCloud(region)
 	if err != nil {
@@ -43,24 +42,25 @@ func NewCloud(region string) *cloud {
 	}
 
 	return &cloud{
-		ec2.New(sess),
-		s3.New(sess),
+		ec2.NewFromConfig(config),
+		s3.NewFromConfig(config),
 		c,
 	}
 }
 
-func (c *cloud) getNodeInstance(clusterName string) (*ec2.Instance, error) {
+func (c *cloud) getNodeInstance(clusterName string) (*ec2types.Instance, error) {
 	request := &ec2.DescribeInstancesInput{
-		Filters: []*ec2.Filter{
+		Filters: []ec2types.Filter{
 			{
 				Name:   aws.String("tag:KubernetesCluster"),
-				Values: []*string{aws.String(clusterName)},
+				Values: []string{clusterName},
 			},
 		},
 	}
 
-	instances := []*ec2.Instance{}
-	response, err := c.ec2client.DescribeInstances(request)
+	instances := []ec2types.Instance{}
+	ctx := context.Background()
+	response, err := c.ec2client.DescribeInstances(ctx, request)
 	if err != nil {
 		return nil, err
 	}
@@ -72,7 +72,7 @@ func (c *cloud) getNodeInstance(clusterName string) (*ec2.Instance, error) {
 		return nil, fmt.Errorf("no instances in cluster %q found", clusterName)
 	}
 
-	return instances[0], nil
+	return &instances[0], nil
 }
 
 func (c *cloud) createS3Bucket(name string) error {
@@ -80,7 +80,8 @@ func (c *cloud) createS3Bucket(name string) error {
 		Bucket: aws.String(name),
 	}
 
-	_, err := c.s3client.CreateBucket(request)
+	ctx := context.Background()
+	_, err := c.s3client.CreateBucket(ctx, request)
 	if err != nil {
 		return err
 	}
@@ -92,14 +93,15 @@ func (c *cloud) deleteS3Bucket(name string) error {
 		Bucket: aws.String(name),
 	}
 
-	_, err := c.s3client.DeleteBucket(request)
+	ctx := context.Background()
+	_, err := c.s3client.DeleteBucket(ctx, request)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func getSecurityGroupIds(node *ec2.Instance) []string {
+func getSecurityGroupIds(node *ec2types.Instance) []string {
 	groups := []string{}
 	for _, sg := range node.SecurityGroups {
 		groups = append(groups, *sg.GroupId)
--- tests/e2e/go.mod
+++ tests/e2e/go.mod
@@ -1,7 +1,10 @@
 module sigs.k8s.io/aws-fsx-csi-driver/tests/e2e
 
 require (
-	github.com/aws/aws-sdk-go v1.44.76
+	github.com/aws/aws-sdk-go-v2 v1.37.1
+	github.com/aws/aws-sdk-go-v2/config v1.29.14
+	github.com/aws/aws-sdk-go-v2/service/ec2 v1.239.0
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.85.1
 	github.com/onsi/ginkgo/v2 v2.21.0
 	github.com/onsi/gomega v1.35.1
 	k8s.io/api v0.33.3
@@ -17,20 +20,22 @@ require (
 	github.com/JeffAshton/win_pdh v0.0.0-20161109143554-76bb4ee9f0ab // indirect
 	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
-	github.com/aws/aws-sdk-go-v2 v1.36.3 // indirect
-	github.com/aws/aws-sdk-go-v2/config v1.29.14 // indirect
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.7.0 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.17.67 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.30 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.34 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.34 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.4.1 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.7.1 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.3 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.4.1 // indirect
 	github.com/aws/aws-sdk-go-v2/service/fsx v1.53.4 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.12.3 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.12.15 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.13.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.8.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.13.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.19.1 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.25.3 // indirect
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.30.1 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.33.19 // indirect
-	github.com/aws/smithy-go v1.22.3 // indirect
+	github.com/aws/smithy-go v1.22.5 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/blang/semver/v4 v4.0.0 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
@@ -69,7 +74,6 @@ require (
 	github.com/gorilla/websocket v1.5.4-0.20250319132907-e064f32e3674 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.24.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/karrick/godirwalk v1.17.0 // indirect
